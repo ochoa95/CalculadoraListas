c805ec81e1829701043f3b8cebc3a86541e35f62
ochoa95 <och14230@uvg.edu.gt>
2015-08-20 22:59:22 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,33 @@
 #main
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/*
+Universidad del Valle de Guatemala
+Algoritmos y Estructura de Datos
+Sección: 10
+Jose Angel Ochoa Carne: 14230
+Diego Sosa. Carné: 14735
+30/07/2015
+Hoja de Trabajo 5
+*/
+
+public class main{
+	
+	public static void main(String[] args) {
+		Calculadora<Double> nCalculadora = new Calculadora<Double>();
+	    
+		try {
+			nCalculadora.first();
+			nCalculadora.readFile("datos.txt");
+		} 	catch(FileNotFoundException e){
+			System.out.println("Error: Fichero no encontrado");
+			System.out.println(e.getMessage());
+		}
+		catch(Exception e){
+			System.out.println("Error de lectura del fichero");
+			System.out.println(e.getMessage());
+		}
+		
+	}
+}
